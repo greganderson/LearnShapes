9f418c5d22312bcf6dae22474e92280456767249
Greg Anderson <gregory.anderson@familysearch.org>
2015-05-26 19:50:25 -0600

    Just finishing getting things all set up.

--- app/src/main/java/com/familybiz/greg/learnshapes/ShapesActivity.java
+++ app/src/main/java/com/familybiz/greg/learnshapes/ShapesActivity.java
@@ -2,6 +2,7 @@ package com.familybiz.greg.learnshapes;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 
 public class ShapesActivity extends Activity {
@@ -9,6 +10,10 @@ public class ShapesActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_shapes);
+        setTheme(android.R.style.Theme_Material);
+
+        final LinearLayout rootLayout = new LinearLayout(this);
+        
+        setContentView(rootLayout);
     }
 }
