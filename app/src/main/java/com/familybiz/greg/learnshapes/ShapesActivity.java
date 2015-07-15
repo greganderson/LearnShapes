7b36f174d044b54013dfce756383d38078f162d9
Greg Anderson <gregory.anderson@familysearch.org>
2015-07-14 20:29:31 -0600

    Added greek letters, now can cycle through

--- app/src/main/java/com/familybiz/greg/learnshapes/ShapesActivity.java
+++ app/src/main/java/com/familybiz/greg/learnshapes/ShapesActivity.java
@@ -2,7 +2,12 @@ package com.familybiz.greg.learnshapes;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import static com.familybiz.greg.learnshapes.R.drawable.beta_u;
 
 
 public class ShapesActivity extends Activity {
@@ -13,7 +18,29 @@ public class ShapesActivity extends Activity {
         setTheme(android.R.style.Theme_Material);
 
         final LinearLayout rootLayout = new LinearLayout(this);
-        
+
+        final Integer[] shapeArray = {
+                R.drawable.beta_u,
+                R.drawable.divide,
+                R.drawable.heart,
+                R.drawable.infinity,
+                R.drawable.ohm,
+                R.drawable.pi_l,
+                R.drawable.sigma_u,
+                R.drawable.triangle
+        };
+
+        final RandomItemList<Integer> shapes = new RandomItemList<>(shapeArray);
+
+        rootLayout.setBackground(getDrawable(shapes.nextItem()));
+
+        rootLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rootLayout.setBackground(getDrawable(shapes.nextItem()));
+            }
+        });
+
         setContentView(rootLayout);
     }
 }
